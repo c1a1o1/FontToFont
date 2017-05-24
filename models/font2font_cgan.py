9aa09e695b3e767784e580a30eea0b8761ae6c15
Peter_Liu <heermasterlp@gmail.com>
2017-05-24 17:26:47 +0800

    add random seed function

--- models/font2font_cgan.py
+++ models/font2font_cgan.py
@@ -389,6 +389,8 @@ class Font2Font(object):
         if not self.sess:
             raise Exception("no session registered")
 
+        tf.set_random_seed(1234)
+
         learning_rate = tf.placeholder(tf.float32, name="learning_rate")
         d_optimizer = tf.train.AdamOptimizer(learning_rate, beta1=0.5).minimize(loss_handle.d_loss, var_list=d_vars)
         g_optimizer = tf.train.AdamOptimizer(learning_rate, beta1=0.5).minimize(loss_handle.g_loss, var_list=g_vars)
