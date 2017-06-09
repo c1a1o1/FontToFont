52b793f29d0ce3bb490069111c74602eb2d9b01e
Peter_Liu <heermasterlp@gmail.com>
2017-06-09 20:12:12 +0800

    fix bug

--- util/uitls.py
+++ util/uitls.py
@@ -44,6 +44,7 @@ def read_split_image(img):
 
 
 def shift_and_resize_image(img, shift_x, shift_y, nw, nh):
+    print("img.shape:{}".format(img.shape))
     w, h, _ = img.shape
     enlarged = misc.imresize(img, [nw, nh])
     return enlarged[shift_x:shift_x + w, shift_y:shift_y + h]
